0d7e4a2b749b9f15f6fab1e6a84847a7d056d5b6
K. Bias <91326397+TKDzKen@users.noreply.github.com>
2025-10-15 21:56:46 -0400

    Enhance Scanner with IP validation and more robust error handling

--- Scanner.py
+++ Scanner.py
@@ -2,6 +2,7 @@ import socket
 import argparse
 import threading
 from Scanner_Imports import ping_sweep, single_ping
+import ipaddress
 
 def art():
     ascii_art = r"""
@@ -13,7 +14,6 @@ def art():
                                      \/     \/     \/     \/ 
   """
     print(ascii_art)
-
 
 art()
 print(("__") * 60)
@@ -39,23 +39,43 @@ if __name__ == "__main__":
     parser.add_argument('--ping', type=str, help='Performs a ping against a singular ip address')
     args = parser.parse_args()
 
+    if not args.ip:
+        print("[!] Ip address required to perform scan or ping. use --help for more information.")
+        exit(1)
+
+    try:
+        ipaddress.ip_address(args.ip)
+    except ValueError:
+        print("[!] Invalid IP address. Please provide a valid address.")
+        exit(1)
+        
+    if args.ping:
+        print(f"Pinging Host")
+        single_ping(args.ip)
+        exit(0)
+    if args.PS:
+        print(f"Ping sweeping host ip address")
+        ping_sweep(args.ip)
+        exit(0)
+
     # Multi-Threading
     threads = []
     if args.min_port is None or args.max_port is None:
-        print("[!] Both min_port and max_port must be specified. use --help for more infomation.")
+        print("[!] Both min_port and max_port must be specified. use --help for more information.")
         ans = input("Would you like to test the default ports (Y/N): ")
 
-        if ans in ["Y", "y", "yes", "Yes"]:
-            args.min_port = 1
-            args.max_port = 1000
-        elif ans in ["N", "n", "no", "No"]:
-            print("Exiting...")
-            exit(1)
-        else:
-            exit(1)
+        while True:
+            if ans in ["Y", "y", "yes", "Yes"]:
+                args.min_port = 1
+                args.max_port = 1000
+            elif ans in ["N", "n", "no", "No"]:
+                print("Exiting...")
+                exit(1)
+            else:
+                continue
     
     if args.min_port <= 0 or args.max_port <= 0:
-        print("[!] Ports can not be 0 or less. Use --help for more infomation.")
+        print("[!] Ports can not be 0 or less. Use --help for more information.")
         exit(1)
     for port in range(args.min_port, args.max_port + 1):
         thread = threading.Thread(target=port_scan, args=(args.ip, port))
